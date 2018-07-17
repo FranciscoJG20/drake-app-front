13cbc207ec6b1e4f6b6123a3f143182ebe1024b3
Francisco <fjgutierrez112@gmail.com>
2018-07-17 19:55:12 -0400

    created songlist component

---
+++ src/components/App/SongList/songlist.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class songlist extends Component {
+  render() {
+    return (
+      <div>
+        <p> I am the tracklist </p>
+      </div>
+    );
+  }
+}
+
+export default songlist;
