47a42d53e9dbf7807af8999949ea688cdca87eb7
Francisco <fjgutierrez112@gmail.com>
2018-07-17 19:53:23 -0400

    reorganized app component to render new header

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> Scorpion </h1>
+        </header>
+        <p className="App-intro">
+          This is a song tracklist of Drake's recent album
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
