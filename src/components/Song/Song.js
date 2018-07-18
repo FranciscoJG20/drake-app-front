030e859c8415d529691d869e96b661dd7229a18a
Francisco <fjgutierrez112@gmail.com>
2018-07-17 21:52:23 -0400

    created handleInput method when posting new song

--- src/components/Song/Song.js
+++ src/components/Song/Song.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 
 class Song extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: ""
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         <form>
           <label htmlFor="song">Add Song Here</label>
           <br />
-          <input type="text" placeholder="song name" />
+          <input
+            type="text"
+            placeholder="song name"
+            onChange={this.handleInput}
+          />
           <br />
           <input type="submit" value="submit" />
         </form>
