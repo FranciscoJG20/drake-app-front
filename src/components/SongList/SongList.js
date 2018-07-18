901f59a6e9feca4a9b4a24126b3008f41f904178
Francisco <fjgutierrez112@gmail.com>
2018-07-17 20:34:27 -0400

    initialized state in SongList and rendered two songs

--- src/components/SongList/SongList.js
+++ src/components/SongList/SongList.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import Song from "../Song/Song";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 class songlist extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      songs: ["Emotionless", "Summer Games", "On Your Feelings"]
+    };
+  }
   render() {
     return (
       <div>
-        <p> I am the tracklist! </p>
+        <h2> Tracklist </h2>
+        <br />
+        {this.state.songs[0]}
+        <br />
+        {this.state.songs[1]}
+        <br />
+        <br />
         <Song />
         <Route path="/song" component={Song} />
       </div>
