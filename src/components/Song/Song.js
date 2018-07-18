35f8a32ea6603c99f1e6be57fa358e2ab446739b
Francisco <fjgutierrez112@gmail.com>
2018-07-17 20:03:26 -0400

    created Song component

---
+++ src/components/Song/Song.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Song extends Component {
+  render() {
+    return (
+      <div>
+        <p> I am one song </p>
+      </div>
+    );
+  }
+}
+
+export default Song;
