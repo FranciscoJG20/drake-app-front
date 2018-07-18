463b0408d6f6ef754ce9a6165214040ee057ead8
Francisco <fjgutierrez112@gmail.com>
2018-07-17 20:27:55 -0400

    required Route and Link , and set the route to create a new song

--- src/components/SongList/SongList.js
+++ src/components/SongList/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Song from "../Song/Song";
+import { Route, Link } from "react-router-dom";
 
 class songlist extends Component {
   render() {
@@ -7,6 +8,8 @@ class songlist extends Component {
       <div>
         <p> I am the tracklist! </p>
         <Song />
+        <Link to="/addsong">Add song</Link>
+        <Route path="/song" component={Song} />
       </div>
     );
   }
